6722ec71683c41ce8a1aeb741cedc9df5c6d41b9
Grace <grace941217@gmail.com>
2022-11-01 00:45:39 -0400

    commit tree c156e9dbada9e759fe94781fb5861662b01eda90

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+end
+
+# word = Anagram.new("listen")
+diaper = Anagram.new("diaper")
+diaper.match(%w[hello world zombies pants dipper])
